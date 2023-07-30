181600b2702487a5a8be21c17f890ef247ff8d13
Daniel Maestre <dmaestre1490@gmail.com>
2023-07-30 15:02:56 -0500

    aja

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Videojuegos)
+admin.site.register(Peliculas)
+admin.site.register(Clientes)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -3,25 +3,34 @@ from django.db import models
 # Create your models here.
 
 generos_peliculas = (
-    ("DRAMA", "Drama")
-    ("ACCION", "Acción")
-    ("AVENTURA", "Aventura")
-    ("COMEDIA", "Comedia")
-    ("TERROR", "Terror")
-    ("OTROS", "Otros")
+    ("DRAMA", "Drama"),
+    ("ACCION", "Acción"),
+    ("AVENTURA", "Aventura"),
+    ("COMEDIA", "Comedia"),
+    ("TERROR", "Terror"),
+    ("OTROS", "Otros"),
  )
 
 generos_videojuegos = (
-        ("FPS", "FPS")
-        ("ACCION", "Acción")
-        ("AVENTURA", "Aventura")
-        ("ESTRATEGIA", "Estrategia")
-        ("TERROR", "Terror")
-        ("OTROS", "Otros")
+    ("FPS", "FPS"),
+    ("ACCION", "Acción"),
+    ("AVENTURA", "Aventura"),
+    ("ESTRATEGIA", "Estrategia"),
+    ("TERROR", "Terror"),
+    ("OTROS", "Otros"),
     )
 
+consolas = (
+    ("PLAYSTATION_4", "PlayStation 4"),
+    ("PLAYSTATION_5", "PlayStation 5"),
+    ("NINTENDO_SWITCH", "Nintendo Switch"),
+    ("XBOX_ONE", "Xbox One"),
+    ("PC", "PC"),
+    ("OTROS", "Otros"),
+)
 
-class peliculas(models.Model):
+
+class Peliculas(models.Model):
     
     titulo = models.CharField(max_length=50)
     director = models.CharField(max_length=50)
@@ -31,20 +40,31 @@ class peliculas(models.Model):
     fecha_estreno = models.DateField()
     cantidad = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.titulo}"
 
 
-class videojuegos(models.Model):
+
+class Videojuegos(models.Model):
     titulo = models.CharField(max_length=50)
     director = models.CharField(max_length=50)
     productora = models.CharField(max_length=50)
+    actor_protagonista = models.CharField(max_length=50)
     genero = models.CharField(max_length=20, choices=generos_videojuegos)
     fecha_estreno = models.DateField()
     cantidad = models.IntegerField()
+    consolas = models.CharField(max_length=20, choices=consolas)
 
-class clientes(models.Model):
+    def __str__(self):
+        return f"{self.titulo}, ({self.consolas})"
+
+class Clientes(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     direccion = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
 
 
